155039fd003c961e9373d5af48915fe837716edf
Joel Kaltman <joel@sandbox.game>
2024-01-12 16:14:00 -0300

    Native share

--- Assets/Scripts/Share.cs
+++ Assets/Scripts/Share.cs
@@ -3,65 +3,36 @@ using System.Collections;
 using System.IO;
 using UnityEngine.UI;
 
-public class Share : MonoBehaviour {
-
-	private bool isProcessing = false;
-
+public class Share : MonoBehaviour 
+{
 	public void share()
 	{
-		if(!isProcessing)
-			StartCoroutine( ShareScreenshot() );
+		StartCoroutine( TakeScreenshotAndShare() );
 	}
-
-	public IEnumerator ShareScreenshot()
+	
+	private IEnumerator TakeScreenshotAndShare()
 	{
-		isProcessing = true;
-
-		// wait for graphics to render
 		yield return new WaitForEndOfFrame();
-		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
-		// create the texture
-		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
-		
-		// put buffer into texture
-		screenTexture.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height),0,0);
 
-		// apply
-		screenTexture.Apply();
-		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height ), 0, 0);
+		ss.Apply();
 
-		byte[] dataToSave = screenTexture.EncodeToPNG();
-		
-		string destination = Path.Combine(Application.persistentDataPath,System.DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + ".png");
+		string filePath = Path.Combine(Application.persistentDataPath, System.DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + ".png");
+		File.WriteAllBytes(filePath, ss.EncodeToPNG());
 
-		File.WriteAllBytes(destination, dataToSave);
+		// To avoid memory leaks
+		Destroy(ss);
 
-		if(!Application.isEditor)
-		{
-			// block to open the file and share it ------------START
-			AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-			AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-			intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-			AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-			AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse","file://" + destination);
-			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-			int puntaje = PlayerPrefs.GetInt ("Puntaje");
-			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "I killed " + PlayerStats.Instance.score + " monsters in Another Shooting Game! You think you can beat me? Download it in the playstore and try!");
-			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "ANOTHER SHOOTING GAME");
-			intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
-			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+		new NativeShare().AddFile(filePath)
+			.SetSubject("Another Shooting Game")
+			.SetText("I killed " + PlayerStats.Instance.score + " monsters in ASG! Think you can beat me??")
+			//.SetUrl( "https://github.com/yasirkula/UnityNativeShare" )
+			//.SetCallback( ( result, shareTarget ) => Debug.Log( "Share result: " + result + ", selected app: " + shareTarget ) )
+			.Share();
 
-			// option one WITHOUT chooser:
-			//currentActivity.Call("startActivity", intentObject);
-
-			// option two WITH chooser:
-			AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "YO BRO! WANNA SHARE?");
-			currentActivity.Call("startActivity", jChooser);
-
-			// block to open the file and share it ------------END
-
-		}
-		isProcessing = false;
+		// Share on WhatsApp only, if installed (Android only)
+		//if( NativeShare.TargetExists( "com.whatsapp" ) )
+		//	new NativeShare().AddFile( filePath ).AddTarget( "com.whatsapp" ).Share();
 	}
 }

02cd96f81536f0f7958420b3569b46f573cfc299
Joel Kaltman <joel@sandbox.game>
2024-03-03 21:46:35 -0300

    Batarang pow bam

--- Assets/Scripts/Guns/BatarangCollision.cs
+++ Assets/Scripts/Guns/BatarangCollision.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class BatarangCollision : MonoBehaviour {
+public class BatarangCollision : ServerOnlyMonobehavior {
 
 	public List<GameObject> imagesPow;
 
@@ -11,7 +11,8 @@ public class BatarangCollision : MonoBehaviour {
 		if (col.gameObject.tag == "Enemy") {
 			Vector3 enemyPos = col.gameObject.transform.position;
 			int random = Random.Range (0, imagesPow.Count);
-			Instantiate (imagesPow[random], new Vector3 (enemyPos.x, 6, enemyPos.z - 0.5f), Quaternion.Euler (50, 0, 0));
+			var pow = Instantiate (imagesPow[random], new Vector3 (enemyPos.x, 6, enemyPos.z - 0.5f), Quaternion.Euler (50, 0, 0));
+			pow.GetComponent<NetworkObject>()?.Spawn(true);
 		}
 	}
 }
